1ccb0c03e689511c7a20024da8a34c8d5488d3f6
daria-astahova <amandabrown463@u.boisestate.edu>
2025-11-08 19:00:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,12 +15,18 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/game.html', (req, res) => res.sendFile(path.join(__dirname, 'game.html')));
 
 // // Character stats (example)
-// const characterStats = {
-//     "Daria": { profession: "Game Designer", luck: 4, talent: 3, networking: 2, wealth: 1 },
-//     "Tony": { profession: "Fashion Designer/Icon", luck: 3, talent: 2, networking: 4, wealth: 1 },
-//     "Logan": { profession: "Reality TV Star", luck: 3, talent: 1, networking: 4, wealth: 2 }
-// };
-
+const characterStats = {
+  "Daria": { profession: "Game Designer", luck: 4, talent: 3, networking: 2, wealth: 1 },
+  "Tony": { profession: "Fashion Designer/Icon", luck: 3, talent: 2, networking: 4, wealth: 1 },
+  "Logan": { profession: "Reality TV Star", luck: 3, talent: 1, networking: 4, wealth: 2 },
+  "Raeann": {profession: "Actress" ,luck: 2,talent: 1, networking: 4, wealth: 3},
+  "Paige": {profession: "Writer",luck: 3 ,talent:2 , networking: 1, wealth: 4},
+  "Tegan": {profession: "Singer",luck: 4,talent:1 , networking: 2, wealth: 3},
+  "Adam": {profession: "Streamer",luck: 1 ,talent:3 , networking:4 , wealth: 2},
+  "Sophie": {profession: "Ballerina",luck: 2,talent:4 , networking: 1, wealth:3 },
+  "Aileen": {profession: "Comedian",luck: 2,talent: 4, networking:3 , wealth:1 },
+  "Bailey": {profession: "DJ",luck: 1,talent: 2, networking: 3, wealth:4 },
+};
 // Rooms
 const rooms = {}; // { roomCode: { hostId, players: [], playerRolls: {}, characters: {}, countdown, countdownInterval } }
 
