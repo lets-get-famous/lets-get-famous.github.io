21851856621dc33c62c6d7032081a8ec684cc5ef
daria-astahova <amandabrown463@u.boisestate.edu>
2025-12-03 12:22:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ io.on('connection', (socket) => {
       socket.emit('roomCreated', { roomCode });
       console.log(`🏠 Room ${roomCode} auto-created for host ${socket.id}`);
     }
-  }, 1500);
+  }, 150);
 
   // --- Join room (web player calls this) ---
   socket.on('joinRoom', ({ roomCode, playerName }) => {
